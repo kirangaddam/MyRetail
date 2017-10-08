93028eabe2f872d7218a8ce939ca2add09907f29
Gaddam, Kiran <2kiran.g@gmail.com>
2017-10-08 11:20:38 -0500

    added unit

--- src/test/java/com/myRetail/controller/ProductControllerTest.java
+++ src/test/java/com/myRetail/controller/ProductControllerTest.java
@@ -1,22 +1,35 @@
 package com.myRetail.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myRetail.bean.Product;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-servlet-test.xml")
 public class ProductControllerTest {
 	
 	public static final String REST_SERVICE_URI = "http://localhost:8080/";
 
+	@Autowired
+	private ProductController productController;
 	
-	/*@Autowired
-	private ProductController countryController;
+	ObjectMapper mapper = new ObjectMapper();
 
 	@Test
-	public void testGetCountries() {
-		countryController.getProductById(13860428L);
-	}*/
+	public void testGetCountries() throws JsonProcessingException {
+		ResponseEntity<Product> product = productController.getProductById(13860428L);
+		Assert.assertEquals("The Big Lebowski (Blu-ray)", product.getBody().getName());
+		System.out.println(mapper.writeValueAsString(product));
+	}
 
 }
